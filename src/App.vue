eb3590e15c188e11e8752506cb3b0f979e0456b9
sakshamsneh <sakshamsneh@gmail.com>
2020-01-20 20:44:43 +0530

    Add css

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@
 import PeopleSpace from "./PeopleSpace.vue";
 import IssNow from "./IssNow.vue";
 import IssPass from "./IssPass.vue";
-import VueRouter from "vue-router";
 import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
@@ -44,9 +44,35 @@ export default {
 </script>
 
 <style>
-#app {
-  font-size: 18px;
-  font-family: "Roboto", sans-serif;
-  color: blue;
+nav {
+  background-color: blueviolet;
+  font-size: 110%;
+  text-transform: uppercase;
+  font-weight: 800;
+}
+
+a:link {
+  color: white;
+  text-decoration: none;
+  padding: 5px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:visited {
+  color: white;
+  text-decoration: none;
+}
+
+a:hover {
+  color: white;
+  background-color: lavender;
+  color: mediumslateblue;
+}
+
+a:active {
+  color: white;
+  text-decoration: none;
 }
 </style>
